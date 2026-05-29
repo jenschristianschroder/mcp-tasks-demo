6357311ec1a40008a152e356210d1d6b00ec7a7c
Jens Schrøder <jenschristianschroder@gmail.com>
2026-05-29 14:43:03 +0200

    feat: enable Swagger UI in all environments

--- tasks-api/Program.cs
+++ tasks-api/Program.cs
@@ -24,11 +24,8 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
